a8df0be4ba32827d136d16341420915e1d7c837a
Mark Pitchless <markpitchless@gmail.com>
2012-03-04 16:59:58 +0000

    Bring in the code from NuFoo.

--- lib/MooseX/Getopt/Usage.pm
+++ lib/MooseX/Getopt/Usage.pm
@@ -1,102 +1,179 @@
-package MooseX::Getopt::Usage;
 
-use warnings;
-use strict;
+package NuFoo::Role::GetoptUsage;
 
 =head1 NAME
 
-MooseX::Getopt::Usage - The great new MooseX::Getopt::Usage!
-
-=head1 VERSION
-
-Version 0.01
+NuFoo::Role::GetoptUsage - Generate usage message from attribute meta.
 
 =cut
 
+use 5.010;
 our $VERSION = '0.01';
 
+use Moose::Role;
+use MooseX::Method::Signatures;
+use Term::ANSIColor;
+use Term::ReadKey;
+use Text::Wrap;
+
+requires qw(_compute_getopt_attrs _get_cmd_flags_for_attr);
+
+our %Colours = (
+    flag          => ['yellow'],
+    heading       => ['bold'],
+    command       => ['green'],
+    type          => ['magenta'],
+    default_value => ['cyan'],
+);
+
+BEGIN {
+    # Thanks to Hans Dieter Pearcey for this. See Getopt::Long::Descriptive.
+    # Grab prog name before someone decides to change it.
+    my $prog_name;
+    sub _prog_name { @_ ? ($prog_name = shift) : $prog_name }
+    _prog_name(File::Basename::basename($0));
+
+    # Only use color when we are a terminal
+    $ENV{ANSI_COLORS_DISABLED} = 1
+        unless (-t STDOUT) && !exists $ENV{ANSI_COLORS_DISABLED};
+}
+
+method _parse_usage_format ( ClassName|Object $self: Str $fmt ) {
+    $fmt =~ s/%c/colored $Colours{command}, _prog_name()/ieg;
+    $fmt =~ s/%%/%/g;
+    # TODO - Be good to have a include that generates a list of the opts
+    #        %r - required  %a - all  %o - options
+    $fmt =~ s/^(Usage:)/colored $Colours{heading}, "$1"/e;
+    $self->_getopt_colourise(\$fmt);
+    return $fmt;
+}
+
+method _usage_format (ClassName|Str $self:) { "Usage:\n    %c [OPTIONS]"; }
+
+method getopt_usage( ClassName|Object $self: Bool :$no_headings?, Int :$exit? ) {
+    my $headings = $no_headings ? 0 : 1;
+
+    say $self->_parse_usage_format($self->_usage_format) if $headings;
+
+    my @attrs = sort { $a->name cmp $b->name } $self->_compute_getopt_attrs;
+
+    my $max_len = 0;
+    my (@req_attrs, @opt_attrs);
+    foreach (@attrs) {
+        my $len  = length($_->name);
+        $max_len = $len if $len > $max_len;
+        if ( $_->is_required && !$_->has_default && !$_->has_builder ) {
+            push @req_attrs, $_;
+        }
+        else {
+            push @opt_attrs, $_;
+        }
+    }
+
+    my ($w) = GetTerminalSize;
+    local $Text::Wrap::columns = $w -1 || 72;
+    say colored $Colours{heading}, "Required:" if $headings && @req_attrs;
+    $self->_getopt_attr_usage($_, max_len => $max_len ) foreach @req_attrs;
+    say colored $Colours{heading}, "Options:" if $headings && @opt_attrs;
+    $self->_getopt_attr_usage($_, max_len => $max_len ) foreach @opt_attrs;
+
+    exit $exit if defined $exit;
+    return 1;
+}
+
+method _getopt_attr_usage ( ClassName|Object $self: Object $attr, Int :$max_len ) {
+    my ( $flag, @aliases ) = $self->_get_cmd_flags_for_attr($attr);
+    my $label = join " ", map {
+        length($_) == 1 ? "-$_" : "--$_"
+    } ($flag, @aliases);
+
+    my $docs  = "";
+    my $pad   = $max_len + 2 - length($label);
+    my $def   = $attr->has_default ? $attr->default : "";
+    (my $type = $attr->type_constraint) =~ s/(\w+::)*//g;
+    $docs .= colored($Colours{type}, "$type. ") if $type;
+    $docs .= colored($Colours{default_value}, "Default:$def").". "
+        if $def && ! ref $def;
+    $docs  .= $attr->documentation || "";
+    #$docs  .= $attr->documentation ? "\t".$attr->documentation : "";
+
+    my $col1 = "    $label";
+    $col1 .= "".( " " x $pad );
+    my $out = wrap($col1, (" " x ($max_len + 9)), " - $docs" );
+    $self->_getopt_colourise(\$out);
+    say $out;
+}
+
+method _getopt_colourise( ClassName|Object $self: Str|ScalarRef $out ) {
+    my $str = ref $out ? $out : \$out;
+    $$str =~ s/(--?\w+)/colored $Colours{flag}, "$1"/ge;
+    return ref $out ? $out : $$str;
+}
+
+no Moose::Role;
+
+1;
+__END__
 
 =head1 SYNOPSIS
 
-Quick summary of what the module does.
+ use Moose;
 
-Perhaps a little code snippet.
+ with 'MooseX::Getopt::Basic', 'NuFoo::Role::GetoptUsage';
 
-    use MooseX::Getopt::Usage;
+ $self->getopt_usage;
 
-    my $foo = MooseX::Getopt::Usage->new();
-    ...
+=head1 DESCRIPTION
 
-=head1 EXPORT
+Role to use along with L<MooseX::Getopt> to provide a usage printing method
+that inspects your classes meta information to build a coloured usage message.
 
-A list of functions that can be exported.  You can delete this section
-if you don't export anything, such as for a purely object-oriented module.
+=head1 ATTRIBUTES
 
-=head1 SUBROUTINES/METHODS
+=head1 METHODS
 
-=head2 function1
+=head2 getopt_usage( Bool :$no_headings, Int :$exit )
 
-=cut
+Prints the usage message to stdout followed by a table of the options. Options
+are printed required first, then optional.  These two sections get a heading
+unless C<no_headings> arg is true.
 
-sub function1 {
-}
+If stdout is a tty usage message is colourised. Setting the env var
+ANSI_COLORS_DISABLED will disable colour even on a tty.
 
-=head2 function2
+If an exit arg is given and defined then this method will exit the program with
+that exit code.
 
-=cut
+=head1 SEE ALSO
 
-sub function2 {
-}
-
-=head1 AUTHOR
-
-Mark Pitchless, C<< <markpitchless at gmail.com> >>
+L<perl>, L<Moose>, L<MooseX::Getopt>.
 
 =head1 BUGS
 
-Please report any bugs or feature requests to C<bug-moosex-getopt-usage at rt.cpan.org>, or through
-the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=MooseX-Getopt-Usage>.  I will be notified, and then you'll
-automatically be notified of progress on your bug as I make changes.
+All complex software has bugs lurking in it, and this module is no exception.
+Please report any bugs or feature requests via the github page at:
 
-
-
+L<http://github.com/markpitchless/moosex-getopt-usage>
 
 =head1 SUPPORT
 
 You can find documentation for this module with the perldoc command.
 
-    perldoc MooseX::Getopt::Usage
+    perldoc NuFoo
 
+The source code and other information is hosted on github:
 
-You can also look for information at:
+L<http://github.com/markpitchless/moosex-getopt-usage>
 
-=over 4
+=head1 AUTHOR
 
-=item * RT: CPAN's request tracker
-
-L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=MooseX-Getopt-Usage>
-
-=item * AnnoCPAN: Annotated CPAN documentation
-
-L<http://annocpan.org/dist/MooseX-Getopt-Usage>
-
-=item * CPAN Ratings
-
-L<http://cpanratings.perl.org/d/MooseX-Getopt-Usage>
-
-=item * Search CPAN
-
-L<http://search.cpan.org/dist/MooseX-Getopt-Usage/>
-
-=back
-
+Mark Pitchless, C<< <markpitchless at gmail.com> >>
 
 =head1 ACKNOWLEDGEMENTS
 
+=head1 COPYRIGHT & LICENSE
 
-=head1 LICENSE AND COPYRIGHT
-
-Copyright 2012 Mark Pitchless.
+Copyright 2012 Mark Pitchless
 
 This program is free software; you can redistribute it and/or modify it
 under the terms of either: the GNU General Public License as published
@@ -104,7 +181,4 @@ by the Free Software Foundation; or the Artistic License.
 
 See http://dev.perl.org/licenses/ for more information.
 
-
 =cut
-
-1; # End of MooseX::Getopt::Usage
