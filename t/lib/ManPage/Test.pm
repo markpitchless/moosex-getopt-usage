f88c8cc7f196a9eccad3921a5d0d54cb90b59cb2
Mark Pitchless <markpitchless@gmail.com>
2012-03-18 03:47:37 +0000

    More tests.

--- t/lib/ManPage/Test.pm
+++ t/lib/ManPage/Test.pm
@@ -9,6 +9,7 @@ use Test::More;
 use Capture::Tiny qw(:all);
 use Test::Differences;
 use File::Slurp qw(slurp);
+use File::Basename;
 
 use FindBin qw($Bin);
 our $TBin = "$Bin/bin";
@@ -17,35 +18,37 @@ sub startup : Test(startup => 1) {
     use_ok('ManPage');
 }
 
-# TODO: Current man page implimentation doesn't support getting the man page
-# back as a string as we hand off to Pod::Usage.
-#sub manpage : Test(2) {
-#    my $self = shift;
-#
-#    my $testme = ManPage->new();
-#    ok( $testme, "Construct ManPage" ) or die "No object to test with!";
-#
-#    my $out_ok = <<EOSTDOUT;
-#Usage:
-#    manpage.t [OPTIONS]
-#Options:
-#    --help -? --usage - Bool. Display the usage message and exit
-#    --verbose         - Bool. Say lots about what we do
-#    --greet           - Str. Default=World. Who to say hello to.
-#    --language        - Str. Default=en. Language to greet in.
-#EOSTDOUT
-#    my $out = $testme->getopt_usage;
-#    eq_or_diff $out, $out_ok, "ManPage";
-#}
-
-sub manpage_cmd_line : Test(2) {
+sub manpage : Test(2) {
     my $self = shift;
 
-    my $stdout_ok = slurp("$Bin/manpage.ok");
+    my $testme = ManPage->new();
+    ok( $testme, "Construct ManPage" ) or die "No object to test with!";
+
+    my $out_ok = slurp("$Bin/manpage.getopt_usage.ok");
+    my $out = $testme->getopt_usage;
+    eq_or_diff $out, $out_ok, "ManPage";
+
+    # TODO: Current man page implimentation doesn't support getting the man
+    # page back as a string as we hand off to Pod::Usage.
+}
+
+sub manpage_cmd_line : Test(8) {
+    my $self = shift;
+
+    my $stdout_ok = slurp("$Bin/manpage.man.ok");
     my $stderr_ok = "";
     foreach my $flag (qw/--man/) {
-        my $cmd = "$TBin/manpage $flag";
-        my ($stdout, $stderr) = capture { system($cmd) };
+        my $cmd = "manpage $flag";
+        my ($stdout, $stderr) = capture { system("$TBin/$cmd") };
+        eq_or_diff $stdout, $stdout_ok, "$cmd STDOUT";
+        eq_or_diff $stderr, $stderr_ok, "$cmd STDERR";
+    }
+    
+    $stdout_ok = slurp "$Bin/manpage.usage.ok";
+    $stderr_ok = "";
+    foreach my $flag (qw/-? --help --usage/) {
+        my $cmd = "manpage $flag";
+        my ($stdout, $stderr) = capture { system("$TBin/$cmd") };
         eq_or_diff $stdout, $stdout_ok, "$cmd STDOUT";
         eq_or_diff $stderr, $stderr_ok, "$cmd STDERR";
     }
